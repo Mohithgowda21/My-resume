765494e59951e040a3b73be7808d7f9f94e83365
Mohithgowda21 <mohithgowda2124@gmail.com>
2024-12-08 15:45:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    var imageUrl = 'image.jpg';  // Replace with your image URL
+    $('#dynamicImage').html('<img src="' + imageUrl + '" alt="Dynamic Image">');
+});
